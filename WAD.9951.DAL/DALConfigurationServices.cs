584889a153dd308ff1adda5496043434df16af4e
Zack711 <crazycloun99@gmail.com>
2024-03-07 23:30:44 +0500

    Mapping and configurations

---
+++ WAD.9951.DAL/DALConfigurationServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WAD._9951.DAL.Data;
+using WAD._9951.DAL.Interfaces;
+using WAD._9951.DAL.Models;
+using WAD._9951.DAL.Repositories;
+
+namespace WAD._9951.DAL
+{
+	public static class DALConfigureServices
+	{
+		public static IServiceCollection DALConfigureServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			// Adding connection string to sql
+			services.AddDbContext<FitnessAppDbContext>(options =>
+				options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+			// Depedency injection 
+			services.AddScoped<IRepository<User>, UserRepository>();
+			services.AddScoped<IRepository<FitnessActivity>, FitnessActivityRepository>();
+
+			// Adding profile
+			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+			return services;
+		}
+	}
+}
---
+++ WAD.9951.DAL/Mapping/UserMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAD._9951.DAL.Dtos;
+using WAD._9951.DAL.Models;
+
+namespace WAD._9951.DAL.Mapping
+{
+	public class UserMappings : Profile
+	{
+		public UserMappings()
+		{
+			CreateMap<User, UserDto>().ReverseMap();
+		}
+	}
+}
