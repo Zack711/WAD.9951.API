f04e2643a6edec852304b8d9159ba46ec442760b
Zack711 <crazycloun99@gmail.com>
2024-03-07 23:38:37 +0500

    And lastly was added controllers

---
+++ WAD.9951.API/Controllers/FitnessActivityController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD._9951.DAL.Dtos;
+using WAD._9951.DAL.Models;
+using WAD._9951.DAL.Repositories;
+
+namespace WAD._9951.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FitnessActivityController : ControllerBase
+	{
+		private readonly FitnessActivityRepository _activityRepository;
+		private readonly IMapper _mapper;
+
+		public FitnessActivityController(FitnessActivityRepository activityRepository, IMapper mapper)
+		{
+			_activityRepository = activityRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public ActionResult<List<FitnessActivityDto>> GetAllActivities()
+		{
+			var activities = _activityRepository.GetAll();
+			var activityDtos = _mapper.Map<List<FitnessActivityDto>>(activities);
+			return Ok(activityDtos);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<FitnessActivityDto> GetActivityById(int id)
+		{
+			var activity = _activityRepository.GetById(id);
+			if (activity == null)
+			{
+				return NotFound();
+			}
+			var activityDto = _mapper.Map<FitnessActivityDto>(activity);
+			return Ok(activityDto);
+		}
+
+		[HttpPost]
+		public ActionResult<FitnessActivityDto> CreateActivity(FitnessActivityDto activityDto)
+		{
+			var activity = _mapper.Map<FitnessActivity>(activityDto);
+			_activityRepository.Add(activity);
+			activityDto.Id = activity.Id; // Update the DTO with the newly generated ID
+			return Ok(activityDto);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult UpdateActivity(int id, FitnessActivityDto activityDto)
+		{
+			if (id != activityDto.Id)
+			{
+				return BadRequest();
+			}
+
+			var existingActivity = _activityRepository.GetById(id);
+			if (existingActivity == null)
+			{
+				return NotFound();
+			}
+
+			var activity = _mapper.Map<FitnessActivity>(activityDto);
+			_activityRepository.Update(activity);
+
+			return Ok("Updated");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteActivity(int id)
+		{
+			var activity = _activityRepository.GetById(id);
+			if (activity == null)
+			{
+				return NotFound();
+			}
+
+			_activityRepository.Delete(id);
+			return Ok("Deleted");
+		}
+	}
+}
---
+++ WAD.9951.API/Controllers/UserController.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD._9951.DAL.Dtos;
+using WAD._9951.DAL.Models;
+using WAD._9951.DAL.Repositories;
+
+namespace WAD._9951.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly UserRepository _userRepository;
+		private readonly IMapper _mapper;
+
+		public UserController(UserRepository userRepository, IMapper mapper)
+		{
+			_userRepository = userRepository;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public ActionResult<List<UserDto>> GetAllUsers()
+		{
+			var users = _userRepository.GetAll();
+			var userDtos = _mapper.Map<List<UserDto>>(users);
+			return Ok(userDtos);
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<UserDto> GetUserById(int id)
+		{
+			var user = _userRepository.GetById(id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+			var userDto = _mapper.Map<UserDto>(user);
+			return Ok(userDto);
+		}
+
+		[HttpPost]
+		public ActionResult<UserDto> CreateUser(UserDto userDto)
+		{
+			var user = _mapper.Map<User>(userDto);
+			_userRepository.Add(user);
+			userDto.Id = user.Id; // Update the DTO with the newly generated ID
+			return Ok(userDto);
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult UpdateUser(int id, UserDto userDto)
+		{
+			if (id != userDto.Id)
+			{
+				return BadRequest();
+			}
+
+			var existingUser = _userRepository.GetById(id);
+			if (existingUser == null)
+			{
+				return NotFound();
+			}
+
+			var user = _mapper.Map<User>(userDto);
+			_userRepository.Update(user);
+
+			return Ok("Updated");
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult DeleteUser(int id)
+		{
+			var user = _userRepository.GetById(id);
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			_userRepository.Delete(id);
+			return Ok("Deleted");
+		}
+	}
+}
