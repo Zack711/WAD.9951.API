457de62fec791bfdf75b133adcb42e41692e1a9d
Zack711 <crazycloun99@gmail.com>
2024-03-07 23:30:51 +0500

    Repositories

---
+++ WAD.9951.DAL/Repositories/FitnessActivityRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAD._9951.DAL.Data;
+using WAD._9951.DAL.Interfaces;
+using WAD._9951.DAL.Models;
+
+namespace WAD._9951.DAL.Repositories
+{
+	public class FitnessActivityRepository : IRepository<FitnessActivity>
+	{
+		private readonly FitnessAppDbContext _dbContext;
+
+		public FitnessActivityRepository(FitnessAppDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public List<FitnessActivity> GetAll()
+		{
+			return _dbContext.FitnessActivities.ToList();
+		}
+
+		public FitnessActivity GetById(int id)
+		{
+			return _dbContext.FitnessActivities.FirstOrDefault(a => a.Id == id);
+		}
+
+		public void Add(FitnessActivity entity)
+		{
+			_dbContext.FitnessActivities.Add(entity);
+			_dbContext.SaveChanges();
+		}
+
+		public void Update(FitnessActivity entity)
+		{
+			_dbContext.FitnessActivities.Update(entity);
+			_dbContext.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			var activity = GetById(id);
+			if (activity != null)
+			{
+				_dbContext.FitnessActivities.Remove(activity);
+				_dbContext.SaveChanges();
+			}
+		}
+	}
+}
---
+++ WAD.9951.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WAD._9951.DAL.Data;
+using WAD._9951.DAL.Interfaces;
+using WAD._9951.DAL.Models;
+
+namespace WAD._9951.DAL.Repositories
+{
+	public class UserRepository : IRepository<User>
+	{
+		private readonly FitnessAppDbContext _dbContext;
+
+		public UserRepository(FitnessAppDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public List<User> GetAll()
+		{
+			return _dbContext.Users.ToList();
+		}
+
+		public User GetById(int id)
+		{
+			return _dbContext.Users.FirstOrDefault(u => u.Id == id);
+		}
+
+		public void Add(User entity)
+		{
+			_dbContext.Users.Add(entity);
+			_dbContext.SaveChanges();
+		}
+
+		public void Update(User entity)
+		{
+			_dbContext.Users.Update(entity);
+			_dbContext.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			var user = GetById(id);
+			if (user != null)
+			{
+				_dbContext.Users.Remove(user);
+				_dbContext.SaveChanges();
+			}
+		}
+	}
+}
