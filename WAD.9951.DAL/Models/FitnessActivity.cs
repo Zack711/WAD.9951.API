f8def972191a84ed485a75f0dc2cffcc7d781ccb
Zack711 <crazycloun99@gmail.com>
2024-03-07 23:17:54 +0500

    Adding models of the application and adding DAL project

---
+++ WAD.9951.DAL/Models/FitnessActivity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAD._9951.DAL.Models
+{
+	public class FitnessActivity
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public User User { get; set; }
+		public DateTime Date { get; set; }
+		public string ActivityType { get; set; }
+		public int DurationInMinutes { get; set; }
+		public decimal CaloriesBurned { get; set; }
+	}
+}
