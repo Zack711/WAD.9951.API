f17b6128d184f6a93209931173ac56e46a0c078b
Zack711 <crazycloun99@gmail.com>
2024-03-07 23:25:30 +0500

    Data transware models

---
+++ WAD.9951.DAL/Dtos/FitnessActivityDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WAD._9951.DAL.Dtos
+{
+	public class FitnessActivityDto
+	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public DateTime Date { get; set; }
+		public string ActivityType { get; set; }
+		public int DurationInMinutes { get; set; }
+		public decimal CaloriesBurned { get; set; }
+	}
+}
